205e4a486a2f14f737b65ee2443f74a89be4a7fe
hermandi21 <4ndr34s01@gmail.com>
2023-05-06 13:14:49 +0200

    first commit

---
+++ VersuchNr2/history.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Mon Nov 14 14:12:43 2022)---
+pip install opencv-python
+runfile('C:/Users/ds-05/.spyder-py3/temp.py', wdir='C:/Users/ds-05/.spyder-py3')
+runfile('C:/Users/ds-05/.spyder-py3/temp.py', wdir='C:/Users/ds-05/.spyder-py3')
+
+## ---(Mon Jan  9 14:07:41 2023)---
+runfile('C:/Users/ds-05/Desktop/redlab.py', wdir='C:/Users/ds-05/Desktop')
+reterz
+runcell(0, 'C:/Users/ds-05/Desktop/redlab.py')
+runfile('C:/Users/ds-05/Desktop/redlab.py', wdir='C:/Users/ds-05/Desktop')
+
+## ---(Mon Apr 24 15:05:26 2023)---
+runfile('C:/Users/ds-05/.spyder-py3/temp.py', wdir='C:/Users/ds-05/.spyder-py3')
+runfile('C:/Users/ds-05/.spyder-py3/temp.py', wdir='C:/Users/ds-05/.spyder-py3')
+runfile('C:/Users/ds-05/.spyder-py3/temp.py', wdir='C:/Users/ds-05/.spyder-py3')
+runfile('C:/Users/ds-05/.spyder-py3/temp.py', wdir='C:/Users/ds-05/.spyder-py3')
---
+++ VersuchNr2/temp.py
@@ -0,0 +1,46 @@
+import numy as np
+import time
+import cv2
+
+
+cam = cv2.VideoCapture(0)
+cam.set(3,640)
+cam.set(4,480)
+cam.set(10,150)
+cam.set(12,32)
+
+
+
+
+print("frame width: " + str(cam.get(3)))
+print("frame height: " + str(cam.get(4)))
+print("--------------------------------")
+print("brightness: " + str(cam.get(10)))
+print("contrast: " + str(cam.get(11)))
+print("saturation: " + str(cam.get(12)))
+print("--------------------------------")
+print("gain: " + str(cam.get(14)))
+print("exposure: " + str(cam.get(15)))
+print("--------------------------------")
+print("white balance: " + str(cam.get(17)))
+
+#Routine zur Aufnahme der Bilder
+counter = 0
+i = 0
+while(True):
+    ret, frame = cam.read()
+    if ret == True:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('frame', gray)
+        key = cv2.waitKey(1)
+        if key == ord('s'):
+            cv2.imwrite('schwarzBild' + str(i) + '.png', gray)
+            i = i+1
+            time.sleep(1)
+        if key == ord('q'):
+            break;
+      
+
+cam.release()
+cv2.destroyAllWindows()
+
